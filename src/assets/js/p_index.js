e5bc9f3254c965f5a161932b6e04959196ef204b
JimmyWu <gmwu185@gmail.com>
2022-05-15 00:10:21 +0800

    增加圖示素材與處理 body 基本字型

--- src/assets/js/p_index.js
+++ src/assets/js/p_index.js
@@ -2,25 +2,25 @@
 // console.log('jQuery-Obj', jQuery, 'jQuery -> $', $);
 // console.log('index.js');
 
-let app = new Vue({
-  el: '#app',
-  data: {
-    ajaxData: null,
-  },
-  mounted() {
-    let vm = this;
-    $.ajax({
-      url: 'https://raw.githubusercontent.com/hexschool/KCGTravel/master/datastore_search.json',
-      method: 'GET',
-      dataType: 'json',
-      success: function (res) {
-        // console.log('$.ajax -> res -> data', res.result.records);
-        // console.log('$.ajax this', this);
-        vm.ajaxData = res.result.records;
-      },
-      error: function (err) {
-        console.log(err);
-      },
-    });
-  },
-});
+// let app = new Vue({
+//   el: '#app',
+//   data: {
+//     ajaxData: null,
+//   },
+//   mounted() {
+//     let vm = this;
+//     $.ajax({
+//       url: 'https://raw.githubusercontent.com/hexschool/KCGTravel/master/datastore_search.json',
+//       method: 'GET',
+//       dataType: 'json',
+//       success: function (res) {
+//         // console.log('$.ajax -> res -> data', res.result.records);
+//         // console.log('$.ajax this', this);
+//         vm.ajaxData = res.result.records;
+//       },
+//       error: function (err) {
+//         console.log(err);
+//       },
+//     });
+//   },
+// });
